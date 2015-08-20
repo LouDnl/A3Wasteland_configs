753b5ffb77d9a512c693fe70a6df9b9530e6116e
LouDnl <thierry@smeekes.nl>
2015-08-20 12:38:42 +0200

    For recent updates to NL-United mission

---
+++ SQL commands.sql
@@ -0,0 +1,9 @@
+ALTER TABLE `a3wasteland`.`playersave` 
+ADD COLUMN `UniformTexture` VARCHAR(4096) NOT NULL DEFAULT '""' COMMENT '' AFTER `WastelandItems`,
+ADD COLUMN `BackpackTexture` VARCHAR(4096) NOT NULL DEFAULT '""' COMMENT '' AFTER `UniformTexture`;
+
+ALTER TABLE `a3wasteland`.`playerstats` 
+ADD COLUMN `GutCount` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '' AFTER `CaptureCount`;
+
+ALTER TABLE `a3wasteland`.`playerstatsmap` 
+ADD COLUMN `GutCount` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '' AFTER `CaptureCount`;
